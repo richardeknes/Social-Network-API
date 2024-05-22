60041190048ad1cc3b0aca674c01512e20f39fcc
Richard Eknes <richardeknes@gmail.com>
2024-05-21 19:17:38 -0500

    filled out part of User model

---
+++ models/User.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/, "You must use a valid email address!"],
+        },
+        thoughts: [
+
+        ],
+        friends: [
+
+        ]
+    }
+)
